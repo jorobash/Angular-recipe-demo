bac97e4fd4e4996c10c89e3f0211e8a2692fd2ac
jorobash <jorkomzx@gmail.com>
2019-04-02 19:45:58 +0300

    add recipe and shopping list component

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+      new Recipe('A first recipe', 'Description for the first recipe', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
